d64ba12a877ac020a7a2237357db92a98f15c63a
AvaVenturino <AvaVenturino@users.noreply.github.com>
2022-04-08 17:15:18 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * This function calculates the area of a triangle.
  */
-function calculate () {
+function calculate() {
   // input
-  const fahrenheit = (document.getElementById("fahrenheit").value)
+  const fahrenheit = document.getElementById("fahrenheit").value
   // process
-  const celsius = (fahrenheit - 32) * 5.0 / 9.0
+  const celsius = ((fahrenheit - 32) * 5.0) / 9.0
   // output
   document.getElementById("celsius").innerHTML =
-    "The temperature in Celsius is: " + celsius.toFixed(2) + ' °C' 
+    "The temperature in Celsius is: " + celsius.toFixed(2) + " °C"
 }
